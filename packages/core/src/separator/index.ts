9479a3fceb4b9d71febc73639acdc6618b5f50cc
Emmanuel Raymond <peoray@yahoo.com>
2026-04-03 10:41:00 +0100

    doc(separator): add Separator documentation and demos (#34)

--- packages/core/src/separator/index.ts
+++ packages/core/src/separator/index.ts
@@ -1,2 +1,3 @@
 export { default as Separator } from './Separator.vue'
 export type { SeparatorProps, SeparatorState } from './Separator.vue'
+export { SeparatorDataAttributes } from './SeparatorDataAttributes'
